84d45d8f5a11f9a6ef658bdd62f096f7ed8806c3
kosmit10 <kosmit1001@gmail.com>
2024-06-24 00:07:46 +0200

    add startview - no final

---
+++ Wake on time/Start_View/startview.swift
@@ -0,0 +1,18 @@
+//
+//  startview.swift
+//  Wake on time
+//
+//  Created by maciura on 24/06/2024.
+//
+
+import SwiftUI
+
+struct startview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    startview()
+}
