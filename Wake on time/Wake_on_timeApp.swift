319bbe8de2feb9294c5c7216407015257a3eb129
kosmit10 <kosmit1001@gmail.com>
2024-06-23 14:40:59 +0200

    Initial Commit

---
+++ Wake on time/Wake_on_timeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wake_on_timeApp.swift
+//  Wake on time
+//
+//  Created by maciura on 23/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct Wake_on_timeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Wake on timeTests/Wake_on_timeTests.swift
@@ -0,0 +1,16 @@
+//
+//  Wake_on_timeTests.swift
+//  Wake on timeTests
+//
+//  Created by maciura on 23/06/2024.
+//
+
+import Testing
+
+struct Wake_on_timeTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
