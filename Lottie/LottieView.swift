2e30ccb5821b42fb94f1635fc8cafc345fb2b0ec
kosmit10 <kosmit1001@gmail.com>
2024-06-25 10:53:13 +0200

    startview - final, add Lottie animation and Rive

---
+++ Lottie/LottieView.swift
@@ -0,0 +1,18 @@
+//
+//  LottieView.swift
+//  Wake on time
+//
+//  Created by maciura on 24/06/2024.
+//
+
+import SwiftUI
+
+struct LottieView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LottieView()
+}
---
+++ Lottie/LottieView2.swift
@@ -0,0 +1,8 @@
+//
+//  LottieView2.swift
+//  Wake on time
+//
+//  Created by maciura on 24/06/2024.
+//
+
+import Foundation
